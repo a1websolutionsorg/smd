e1b32c0e236a05b084e3120f7fb90379e881576d
Nitish <a1websolutionsorg@gmail.com>
2023-03-27 16:47:01 +0530

    script file changes

--- Public/Scripts/script.js
+++ Public/Scripts/script.js
@@ -1,1 +1,1 @@
-document.addEventListener("contextmenu",disable => disable.preventDefault());
+document.addEventListener("contextmenu",disable => disable.preventDefault());const date = new Date();let year = date.getFullYear();document.getElementById("year").innerHTML = year;
